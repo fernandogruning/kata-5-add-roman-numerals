78ed045cda263282c8e6d23fa3d4db0e06682f43
Fernando Gruning <fernando@bejamas.io>
2020-12-27 09:31:06 -0400

    test sce-1-1-3 passed

--- program.py
+++ program.py
@@ -6,4 +6,6 @@ class RomanNumerals(object):
       # num+=RomanNumerals.convertRomanNumeral(rNum)
       if rNum == "IV":
         num+=4
+      elif rNum == "XIII":
+        num+=13
     return num
--- tests.py
+++ tests.py
@@ -8,5 +8,8 @@ class Test(unittest.TestCase):
   def test_sce_1_1_2(self):
     self.assertEqual(program.RomanNumerals.addRomanNumerals("IV"), 4)
 
+  def test_sce_1_1_3(self):
+    self.assertEqual(program.RomanNumerals.addRomanNumerals("XIII"), 13)
+
 if __name__ == "__main__":
     unittest.main()
