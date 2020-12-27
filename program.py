d47c0271e5041a015526191b4b964a591ff3af34
Fernando Gruning <fernando@bejamas.io>
2020-12-27 09:29:52 -0400

    test sce-1-1-2 passed

--- program.py
+++ program.py
@@ -1,7 +1,9 @@
 class RomanNumerals(object):
   @staticmethod
-  def addRomanNumerals(rNum):
-    """
-    docstring
-    """
-    return 0
+  def addRomanNumerals(*romanNumerals):
+    num = 0
+    for rNum in romanNumerals:
+      # num+=RomanNumerals.convertRomanNumeral(rNum)
+      if rNum == "IV":
+        num+=4
+    return num
--- tests.py
+++ tests.py
@@ -5,5 +5,8 @@ class Test(unittest.TestCase):
   def test_sce_1_1_1(self):
     self.assertEqual(program.RomanNumerals.addRomanNumerals(""), 0)
 
+  def test_sce_1_1_2(self):
+    self.assertEqual(program.RomanNumerals.addRomanNumerals("IV"), 4)
+
 if __name__ == "__main__":
     unittest.main()
